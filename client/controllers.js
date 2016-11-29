3a5dc73216313dfd287a982bb278c20e4ef85e3a
nanarmeen12 <narminshahin@gmail.com>
2016-11-29 15:17:32 -0500

    text styling

--- client/controllers.js
+++ client/controllers.js
@@ -11,8 +11,31 @@ closestCorgi.controller('corgiController', function($scope, $http){
   			// console.log(parsed)
     		$scope.data.pics = parsed.petfinder.pets.pet;
    
-    		console.log($scope.data.pics)
+    		console.log(parsed.petfinder.pets.pet)
     	})
-     }  
+     };
+  $scope.filterFn = function(car)
+{
+    // Do some tests
+
+    if(parsed.pic.description.$t && parsed.pic.name.$t && parsed.pic.media.photos.photo[3].$t)
+    {
+        return true; // this will be listed in the results
+    }
+
+    return false; // otherwise it won't be within the results
+  };
+ $scope.filterFn = function(car)
+{
+    // Do some tests
+
+    if(parsed.petfinder.pets.pet.breeds.length < 2)
+    {
+        return true; // this will be listed in the results
+    }
+
+    return false; // otherwise it won't be within the results
+  };
+
 });
 
