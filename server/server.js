6261d503be6e9435d309fb298e8d6b034c5934e6
nanarmeen12 <narminshahin@gmail.com>
2016-11-29 11:44:36 -0500

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ request.get("http://api.petfinder.com/pet.find?key=b3d52c8692c6896d3eb1ed809a3f5
   res.json(body);
  });
 })
+var port = process.env.PORT || 3000;
+server.listen(port);
 
-server.listen(process.env.PORT || 3000);
-console.log("localhost is happening!")
+console.log("localhost is happening! Listening on port " + port);
